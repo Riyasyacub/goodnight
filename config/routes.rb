0ffe500a74b0dc0e695932cf7e27d56db500cc7e
Riyas Yacub <riyasyacub@gmail.com>
2023-04-20 00:32:29 +0530

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/sleep_reports', to: 'sleeps#sleep_reports', as: :sleep_reports
+  resources :sleeps, only: [:index, :show, :create, :update, :destroy]
+
+  resources :users, only: [:index] do
+    scope module: :users do
+      resources :follows, only: [:index, :create, :destroy]
+    end
+  end
 end
